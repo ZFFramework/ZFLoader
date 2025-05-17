743e1864cd08303e5cb4dc8a30300644fabbebf5
ZSaberLv0 <z@zsaber.com>
2025-05-17 13:43:44 +0800

    update

--- ZFLoader/zfsrc/main.cpp
+++ ZFLoader/zfsrc/main.cpp
@@ -3,26 +3,25 @@
 #include "ZFUIWidget.h"
 #include "ZFLua.h"
 
-ZFMAIN_ENTRY() {
+static void _ZFLoaderEntry(void) {
     ZFCoreArray<ZFPathInfo> extResList;
     extResList.add(ZFPathInfo(ZFPathType_file(), ZFPathForModule()));
     extResList.add(ZFPathInfo(ZFPathType_file(), ZFPathForStorageShared()));
+
+    ZFPathInfo custom;
+    if(ZFPathInfoFromStringT(custom, ZFState::instance()->get("ZFLoaderCustomResExt"))) {
+        extResList.add(custom);
+    }
 
     ZFLogTrim() << "external res: " << extResList;
     for(zfindex i = 0; i < extResList.count(); ++i) {
         ZFResExtPathAdd(extResList[i]);
     }
 
-    ZFInput src = ZFInputForRes("zf.lua");
-    if(src) {
-        ZFLogTrim("redirect to %s", src.callbackId());
-        ZFLuaExecute(src);
-        ZFLuaGC();
-        return;
-    }
-
     zfobj<ZFUIWindow> w;
     w->show();
+    w->layoutParam()->c_margin(20);
+
     zfobj<ZFUITextView> text;
     w->child(text)->c_sizeWrap();
     text->singleLine(zffalse);
@@ -32,6 +31,65 @@ ZFMAIN_ENTRY() {
         ZFPathInfoToStringT(hint, ZFResExtPathList()[i]);
     }
     text->text(hint);
+
+    if(ZFProtocolIsAvailable("ZFUITextEdit")) {
+        zfobj<ZFUITextEditWidget> input;
+        zfobj<ZFUIButtonBasic> btn;
+
+        w
+            ->child(zfobj<ZFUILinearLayout>()
+                ->c_orientation(ZFUIOrientation::e_Top)
+
+                ->child(zfobj<ZFUITextView>()
+                    ->c_text("or, input custom path info:")
+                    ->c_textAlign(ZFUIAlign::e_Left)
+                )->c_widthFill()
+
+                ->child(zfobj<ZFUILinearLayout>()
+                    ->c_orientation(ZFUIOrientation::e_Left)
+
+                    ->child(input
+                        ->c_placeholderProp("text", zfobj<v_zfstring>("custom path info ext"))
+                    )->c_weight(1)->c_widthFill()
+
+                    ->child(btn
+                        ->c_labelProp("text", zfobj<v_zfstring>("OK"))
+                        ->c_viewSizeMin(ZFUISizeCreate(60, 48))
+                    )
+                )->c_widthFill()
+            )->c_widthFill()->c_alignBottom()
+            ;
+
+        ZFLISTENER_1(onClick
+                , zfautoT<ZFUITextEditWidget>, input
+                ) {
+            zfstring key = "ZFLoaderCustomResExt";
+            zfstring text = input->text();
+            if(text) {
+                ZFState::instance()->set(key, text);
+                ZFUIHintShow(zfstr("restart to take effect"));
+            }
+            else {
+                ZFState::instance()->remove(key);
+                ZFUIHintShow("res ext cleared");
+            }
+        } ZFLISTENER_END()
+        btn->onClick(onClick);
+    }
+
+    ZFInput src = ZFInputForRes("zf.lua");
+    if(src) {
+        ZFLogTrim("redirect to %s", src.callbackId());
+        ZFLuaExecute(src);
+        ZFLuaGC();
+    }
+}
+
+ZFMAIN_ENTRY() {
+    ZFLISTENER_0(loadFinish) {
+        _ZFLoaderEntry();
+    } ZFLISTENER_END()
+    ZFState::instance()->load(loadFinish);
 }
 
 ZFMAIN_PARAM_DISPATCH(LuaRunner) {
